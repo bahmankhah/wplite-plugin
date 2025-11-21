580d666cb0aff08d60012dce02fd025b9e9eb09c
Hesam <bahmankhah1@gmail.com>
2025-11-21 15:34:58 +0330

    minor changes

---
+++ src/Providers/ShortcodeServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Providers;
+
+use App\Shortcodes\Hello;
+use WPLite\Provider;
+
+
+class ShortcodeServiceProvider extends Provider
+{
+    public function register() {
+        Hello::register();
+    }
+}
--- src/configs/app.php
+++ src/configs/app.php
@@ -5,6 +5,10 @@ return [
     'global_middlewares'=>[
 
     ],
+    'providers'=>[
+        App\Providers\AppServiceProvider::class,
+        App\Providers\ShortcodeServiceProvider::class,
+    ],
     'version'=>'v1',
     'name'=>'donapp-core',
     'url'=>getenv('APP_URL'),
