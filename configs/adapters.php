f963fae1647cf1d5923429f102dd51e6945d6f76
Hesam <bahmankhah1@gmail.com>
2025-11-21 18:35:48 +0330

    added cache to adapters config

--- configs/adapters.php
+++ configs/adapters.php
@@ -1,5 +1,12 @@
 <?php
 
 return [
-    
+    'cache'=>[
+        'default'=>'transient',
+        'contexts'=>[
+            'transient' => [
+                'context'=> WPLite\Cache\Drivers\Transient::class
+            ]
+        ]
+    ]
 ];
